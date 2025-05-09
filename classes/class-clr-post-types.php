ab11134e9081baa5efd54c6726639f01d83f92b6
Vladyslav Nahornyi <vladyslav@carloud.com>
2025-05-10 01:01:28 +0200

    Register product post type, add acf field group to it, add small css file to editor screen, add constants, set version header

---
+++ classes/class-clr-post-types.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace clr_brands;
+
+class Clr_Post_Types {
+	public static function setup_entities(): void {
+		self::register_products();
+		self::register_product_meta();
+	}
+
+	public static function register_products() {
+		$labels = array(
+			'name'                  => __( 'Products', 'clr-brands' ),
+			'singular_name'         => __( 'Product', 'clr-brands' ),
+			'menu_name'             => __( 'Products', 'clr-brands' ),
+			'name_admin_bar'        => __( 'Product', 'clr-brands' ),
+			'add_new'               => __( 'Add New', 'clr-brands' ),
+			'add_new_item'          => __( 'Add New Product', 'clr-brands' ),
+			'new_item'              => __( 'New Product', 'clr-brands' ),
+			'edit_item'             => __( 'Edit Product', 'clr-brands' ),
+			'view_item'             => __( 'View Product', 'clr-brands' ),
+			'all_items'             => __( 'All Products', 'clr-brands' ),
+			'search_items'          => __( 'Search Products', 'clr-brands' ),
+			'parent_item_colon'     => __( 'Parent Products:', 'clr-brands' ),
+			'not_found'             => __( 'No products found.', 'clr-brands' ),
+			'not_found_in_trash'    => __( 'No products found in Trash.', 'clr-brands' ),
+			'featured_image'        => _x( 'Product Image', 'clr-brands' ),
+			'set_featured_image'    => _x( 'Set product image', 'clr-brands' ),
+			'remove_featured_image' => _x( 'Remove product image', 'clr-brands' ),
+			'use_featured_image'    => _x( 'Use as product image', 'clr-brands' ),
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 20,
+			'menu_icon'          => 'dashicons-cart', // Dashicon
+			'supports'           => array(
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'excerpt',
+				'comments'
+			),
+			'show_in_rest'       => true, // Enable Gutenberg editor
+			'taxonomies'         => array( 'category', 'post_tag' ) // Assign categories and tags
+		);
+
+		register_post_type( 'product', $args );
+	}
+
+	public static function register_product_meta(): void {
+		add_action( 'acf/init', [ __CLASS__, 'acf_add_product_meta' ], -1 );
+	}
+
+	public static function acf_add_product_meta(): void {
+		acf_add_local_field_group( array(
+			'key'                   => 'group_clr_product_meta',
+			'title'                 => 'Product Settings',
+			'fields'                => self::product_meta_config(),
+			'menu_order'            => 0,
+			'position'              => 'normal',
+			'style'                 => 'default',
+			'label_placement'       => 'top',
+			'instruction_placement' => 'label',
+			'active'                => true,
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'product',
+					),
+				),
+			),
+		) );
+	}
+
+	private static function product_meta_config(): array {
+		$fields = [
+			// Image (ACF Image Picker)
+			[
+				'key'   => 'product_image',
+				'label' => 'Image',
+				'name'  => 'product_image',
+				'type'  => 'image',
+			],
+			// Safety Ingredients (Text Area)
+			[
+				'key'   => 'safety_ingredients',
+				'label' => 'Safety Ingredients',
+				'name'  => 'safety_ingredients',
+				'type'  => 'textarea',
+			],
+			// SDS (File Picker)
+			[
+				'key'   => 'sds',
+				'label' => 'SDS',
+				'name'  => 'sds',
+				'type'  => 'file',
+			],
+			// Tech Data Sheets (File Picker)
+			[
+				'key'   => 'tech_data_sheets',
+				'label' => 'Tech Data Sheets',
+				'name'  => 'tech_data_sheets',
+				'type'  => 'file',
+			],
+			// Product Sell Sheets (File Picker)
+			[
+				'key'   => 'product_sell_sheets',
+				'label' => 'Product Sell Sheets',
+				'name'  => 'product_sell_sheets',
+				'type'  => 'file',
+			],
+			// How To Use (File Picker)
+			[
+				'key'   => 'how_to_use',
+				'label' => 'How To Use',
+				'name'  => 'how_to_use',
+				'type'  => 'file',
+			],
+			// Product Use (Checkbox)
+			[
+				'key'     => 'product_use',
+				'label'   => 'Product Use',
+				'name'    => 'product_use',
+				'type'    => 'checkbox',
+				'choices' => [/* Add options here */ ],
+			],
+			// Rooms (Checkbox)
+			[
+				'key'     => 'rooms',
+				'label'   => 'Rooms',
+				'name'    => 'rooms',
+				'type'    => 'checkbox',
+				'choices' => [/* Add options here */ ],
+			],
+			// Not Subject to CA Act (True/False)
+			[
+				'key'   => 'not_subject_to_ca_act',
+				'label' => 'Not Subject to CA Act',
+				'name'  => 'not_subject_to_ca_act',
+				'type'  => 'checkbox',
+			],
+			// Surfaces (Checkbox)
+			[
+				'key'     => 'surfaces',
+				'label'   => 'Surfaces',
+				'name'    => 'surfaces',
+				'type'    => 'checkbox',
+				'choices' => [/* Add options here */ ],
+			],
+			// SDS Spanish (File Picker)
+			[
+				'key'   => 'sds_spanish',
+				'label' => 'SDS Spanish',
+				'name'  => 'sds_spanish',
+				'type'  => 'file',
+			],
+			// Online Available (Checkbox)
+			[
+				'key'     => 'online_available',
+				'label'   => 'Online Available',
+				'name'    => 'online_available',
+				'type'    => 'checkbox',
+				'choices' => [/* Add options here */ ],
+			],
+			// Available Sizes (Checkbox)
+			[
+				'key'     => 'available_sizes',
+				'label'   => 'Available Sizes',
+				'name'    => 'available_sizes',
+				'type'    => 'checkbox',
+				'choices' => [/* Add options here */ ],
+			],
+			// Custom Surfaces Content (WYSIWYG)
+			[
+				'key'   => 'custom_surfaces_content',
+				'label' => 'Custom Surfaces Content',
+				'name'  => 'custom_surfaces_content',
+				'type'  => 'wysiwyg',
+			],
+			// Subhead (Text)
+			[
+				'key'   => 'subhead',
+				'label' => 'Subhead',
+				'name'  => 'subhead',
+				'type'  => 'text',
+			],
+			// UPC Code Fields (Text)
+			[
+				'key'   => 'upc_code',
+				'label' => 'UPC Code',
+				'name'  => 'upc_code',
+				'type'  => 'text',
+			],
+			[
+				'key'   => 'sort_top',
+				'label' => 'Sort Top',
+				'name'  => 'sort_top',
+				'type'  => 'checkbox',
+			],
+			[
+				'key'   => 'is_new_product',
+				'label' => 'Is New Product',
+				'name'  => 'is_new_product',
+				'type'  => 'true_false',
+			],
+			// Custom Size Text (Text Area)
+			[
+				'key'   => 'custom_size_text',
+				'label' => 'Custom Size Text',
+				'name'  => 'custom_size_text',
+				'type'  => 'textarea',
+			],
+			// How To Use Header (Text)
+			[
+				'key'   => 'how_to_use_header',
+				'label' => 'How To Use Header',
+				'name'  => 'how_to_use_header',
+				'type'  => 'text',
+			],
+			// Product Use Proline (True/False)
+			[
+				'key'   => 'product_use_proline',
+				'label' => 'Product Use Proline',
+				'name'  => 'product_use_proline',
+				'type'  => 'true_false',
+			],
+			[
+				'key' => 'field_product_application',
+				'label' => 'Product Application',
+				'name' => 'product_application',
+				'type' => 'checkbox',
+				'choices' => array(
+					// Add application options here
+				),
+			],
+			// Product Type (Checkbox)
+			[
+				'key' => 'field_product_type',
+				'label' => 'Product Type',
+				'name' => 'product_type',
+				'type' => 'checkbox',
+				'choices' => array(
+					// Add type options here
+				),
+			],
+			// Show Proline Safer Choice (True/False)
+			[
+				'key'   => 'show_proline_safer_choice',
+				'label' => 'Show Proline Safer Choice',
+				'name'  => 'show_proline_safer_choice',
+				'type'  => 'true_false',
+			],
+		];
+		return $fields;
+	}
+}
--- clr-brands.php
+++ clr-brands.php
@@ -1,1 +1,78 @@
 <?php
+/**
+ * Plugin Name: CLR Brands
+ * Version: 1.0.0
+ */
+
+namespace clr_brands;
+
+defined( 'ABSPATH' ) || exit;
+
+class ClrBrands {
+
+	private static ?ClrBrands $instance;
+
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ), -1, 1 );
+	}
+
+	public function init(): void {
+		$this->define_constants();
+		$this->autoload_classes();
+		$this->setup_entities();
+
+		/** Enqueue styles and scripts */
+		$this->enqueue_assets();
+	}
+
+	private function define_constants(): void {
+		define( 'CLR_VERSION', get_file_data( __FILE__, array( 'version' => 'Version' ) )['version'] );
+		define( 'CLR_PATH', dirname( __FILE__ ) );
+		define( 'CLR_URL', plugin_dir_url( __FILE__ ) );
+	}
+
+	private function setup_entities(): void {
+		Clr_Post_Types::setup_entities();
+	}
+
+	private function autoload_classes(): void {
+		spl_autoload_register( function ( $class ) {
+			// Omit __NAMESPACE__ as first index, take only class name
+			$className = explode( '\\', $class );
+			$className = $className[1] ?? $className[0];
+			$className = 'class-' . strtolower( $className );
+			$className = str_replace( '_', '-', $className );
+			$class_file = CLR_PATH . '/classes/' . $className . '.php';
+
+			if ( file_exists( $class_file ) ) {
+				require_once $class_file;
+			}
+		} );
+	}
+
+	private function enqueue_assets(): void {
+		add_action( 'admin_enqueue_scripts', array( $this, 'add_admin_assets' ) );
+	}
+
+	public function add_admin_assets(): void {
+		$screen = get_current_screen();
+
+		if ( $screen->is_block_editor ) {
+			wp_enqueue_style( 'clr-editor', CLR_URL . 'assets/css/editor.css', [], CLR_VERSION );
+		}
+
+//		wp_enqueue_script( 'script-name', CLR_URL . '/js/example.js', array(), '1.0.0', true );
+	}
+
+	public static function get_instance(): ClrBrands {
+		if ( empty( self::$instance ) ) {
+			self::$instance = new ClrBrands();
+
+			return self::$instance;
+		}
+
+		return self::$instance;
+	}
+}
+
+ClrBrands::get_instance();
